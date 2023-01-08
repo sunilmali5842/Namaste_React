79361df2a9fa611db42ea534add97a702689ebb4
sunilmali5842 <sunilmali2045@gmail.com>
2023-01-08 17:49:29 +0530

    create and print html using js

---
+++ Assignment-01/app.js
@@ -0,0 +1,10 @@
+// create and print h1 using js
+
+const heading = document.createElement("h1");
+
+heading.innerHTML = "Namaste React using Javascript";
+
+const root = document.getElementById('root');
+
+root.appendChild(heading);
+
