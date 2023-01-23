2f2230aaf071535f3ab07cdd23748559416cff6e
sunilmali5842 <sunilmali2045@gmail.com>
2023-01-24 01:14:25 +0530

    update

--- chapter-06-explore_the_world/src/components/Header.js
+++ chapter-06-explore_the_world/src/components/Header.js
@@ -1,10 +1,11 @@
 import {useState} from "react";
+import Logo from "../assets/images/logo.jpg";
 
 const Title = () => {
     return (
         <a href="/"><img 
         className="logo"
-         alt="logo" src="https://lh3.googleusercontent.com/p/AF1QipMf9w4RomHXrUkQKvrxtPdjp3SLadP05HDzXlH2=w1080-h608-p-no-v0"/>
+         alt="logo" src={Logo}/>
          </a>
 
     )
